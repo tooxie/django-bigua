398924895ca0e8282149719fecb77d7680d96e96
Alvaro Mouriño <alvaro@mourino.net>
2007-11-12 22:36:26 +0000

    Agregados los nuevos javascript.

---
+++ media/js/functions.js
@@ -0,0 +1,44 @@
+/* JavaScript functions */
+function _(element_id) {
+    return document.getElementById(element_id);
+}
+
+function switch_visibility(obj, dis) {
+    if (obj.style != null) {
+        if (obj.style.display == 'none') {
+            obj.style.display = dis;
+        } else {
+            obj.style.display = 'none';
+        }
+    } else {
+        obj.style.display = 'none';
+    }
+    return true;
+}
+
+function atLoadTime() {
+        $("#hoy").toggle();
+        $("#maniana").toggle();
+        _('hoy_link').innerHTML = '<a href="#" id="hoy_switch">' + _('hoy_link').innerHTML + '</td>';
+        _('maniana_link').innerHTML = '<a href="#" id="maniana_switch">' + _('maniana_link').innerHTML + '</td>';
+}
+
+var bigua_rules = {
+    'body' : function(element) {
+        element.onload = atLoadTime();
+    },
+    'a#hoy_switch' : function(element) {
+        element.onclick = function() {
+            $("#hoy").addClass("ohmy").toggle("slow");
+            return false;
+        }
+    },
+    'a#maniana_switch' : function(element) {
+        element.onclick = function() {
+            $("#maniana").addClass("ohmy").toggle("slow");
+            return false;
+        }
+    }
+};
+
+Behaviour.register(bigua_rules);
