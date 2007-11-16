fcaa6efc35a24589483034921bd93d80ed399d4f
Alvaro Mouriño <alvaro@mourino.net>
2007-11-16 19:11:03 +0000

    Agrego un local_settings.py modelo.

---
+++ local_settings-customize.py
@@ -0,0 +1,20 @@
+# coding=UTF-8
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Your Name', 'name@email.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'bigua'
+DATABASE_USER = 'user'
+DATABASE_PASSWORD = 'password'
+
+TIME_ZONE = 'Uruguay/America'
+
+USE_I18N = True
+
+AUTH_PROFILE_MODULE = 'canchas.Socio'
