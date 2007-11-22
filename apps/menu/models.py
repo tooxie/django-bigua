cf2ace7f40ef4502ba7c61e9a470b06992228f78
Alvaro Mouriño <alvaro@mourino.net>
2007-11-22 20:10:59 +0000

    django-menuse no era traducible, ahora si.

--- apps/menu/models.py
+++ apps/menu/models.py
@@ -2,22 +2,30 @@
 from django.db import models
 from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import ugettext_lazy as _
+from multilingual.translation import Translation
 
 # Create your models here.
 class Link(models.Model):
-    nombre = models.CharField(_(u'nombre'), maxlength=20)
     pagina = models.ForeignKey(FlatPage, blank=True, null=True)
     url = models.CharField(_(u'dirección'), maxlength=255, help_text=_(u'Puede elegir una página ya creada o un sitio en Internet, por ej "http://www.brasil.gov.br/". Si elige ámbas se le dará preferencia a la página.'), blank=True)
     padre = models.ForeignKey('Link', null=True, blank=True, help_text=_(u'Si elige otro link en esta lista, este item del menú se mostrará como un subitem del elegido.'))
     posicion = models.PositiveIntegerField(_(u'posición en lista'), default=1)
     desactivar = models.BooleanField(_(u'desactivar'), help_text=_(u'Si desactiva un link no se mostrará en el menú pero no será borrado.'))
+
+    class Translation(Translation):
+        nombre = models.CharField(_(u'Nombre'), maxlength=20,
+            help_text=_(u'Texto que se va a mostrar en el menú.'))
+
     class Meta:
-        ordering = ('posicion','nombre')
+        ordering = ('posicion',)
+
     class Admin:
-        list_filter = ('nombre',)
-        search_fields = ('nombre', 'pagina', 'url')
+        list_filter = ('pagina','url','desactivar',)
+        search_fields = ('pagina', 'url',)
+
     def __str__(self):
         return self.nombre
+
     def get_href(self):
         if self.pagina:
             return self.pagina.url
@@ -36,8 +44,11 @@ class Menu(models.Model):
         verbose_name = _(u'menu')
         verbose_name_plural = _(u'menues')
         ordering = ('nombre',)
+
     class Admin:
         list_filter = ('nombre',)
         search_fields = ('nombre','descripcion')
+
     def __str__(self):
         return self.nombre
+
